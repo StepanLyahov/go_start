c6760ee51ca54df4cc554310a3a5183ae0cff570
Ляхов Степан <lyakhov.sr@reliab.tech>
2021-04-03 20:00:04 +0300

    feat: add errors and logs

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # go_start
-In this branch I do lesson(https://golang.org/doc/tutorial/create-module)
--- internal/main.go
+++ internal/main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	"go_start/greetings"
+	"log"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(log.Lshortfile)
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	message, error := greetings.Hello("")
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
 	fmt.Println(message)
 }
